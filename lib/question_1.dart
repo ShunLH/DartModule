0e590b0b7e58679e9d3a91e45871704299c19220
Shun Lei Hmu <shunleihmu.shunlh@gmail.com>
2022-07-24 04:02:00 +0630

    first commit

---
+++ lib/question_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+ // 1.
+ List<int> numbersList = [1,2,3,4,5,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+ Set<int> numbersSet = {};
+  
+ numbersSet.addAll(numbersList);
+  
+ var resultList = numbersSet.toList();
+  print(resultList.toString());
+  
+}
+  
